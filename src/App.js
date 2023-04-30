adbdda9ccf1753c1abe47ca59750a7f8690a0477
shinemost <supertain147@163.com>
2023-04-30 23:35:24 +0800

    初始化

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+/*
+ * @LastEditors: shinemost supertain147@163.com
+ * @Date: 2023-04-30 19:35:39
+ * @LastEditTime: 2023-04-30 22:59:06
+ * @FilePath: \react-admin\src\App.js
+ */
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+    
     </div>
   );
 }
